bbf42070df02b4e71a878b5648dc1dbe8e4a824e
chanyu1 <haneul1ij@gmail.com>
2020-12-27 20:30:15 +0900

    create server and client

--- server/index.js
+++ server/index.js
@@ -29,6 +29,10 @@ app.get("/", (req, res) => {
   res.send("Hello World! 안녕하세요~~!");
 });
 
+app.get("/api/hello", (req, res) => {
+  res.send("안뇽~~");
+});
+
 app.post("/api/users/register", (req, res) => {
   // 회원가입할 때 필요한 정보들을 client에서 가져오면 DB에 넣어준다.
   const user = new User(req.body);
