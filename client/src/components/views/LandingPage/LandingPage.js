f6e7db1e98511169978c06267ebcef7b277f690a
chanyu1 <haneul1ij@gmail.com>
2020-12-30 23:07:17 +0900

    create hoc

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,20 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response));
   }, []);
 
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃 실패");
+      }
+    });
+  };
   return (
     <div
       style={{
@@ -17,6 +26,7 @@ function LandingPage() {
       }}
     >
       <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
