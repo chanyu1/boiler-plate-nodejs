bb41dfdf1c7709f227a978ba2d63073414e369df
chanyu1 <haneul1ij@gmail.com>
2020-12-31 13:34:17 +0900

    last commit

--- server/index.js
+++ server/index.js
@@ -47,24 +47,24 @@ app.post("/api/users/register", (req, res) => {
 app.post("/api/users/login", (req, res) => {
   // 요청된 이메일이 DB에 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
-    // 요청된 이메일 유저가 없을 경우.
+    // 요청된 이메일 유저가 없을 경우
     if (!user) {
       return res.json({
         loginSuccess: false,
         message: "The user could not be found.",
       });
     }
-    // 요청된 이메일이 DB에 있다면, 비밀번호가 맞는지 확인.
+    // 요청된 이메일이 DB에 있다면, 비밀번호가 맞는지 확인
     user.comparePassword(req.body.password, (err, isMatch) => {
       if (!isMatch)
         return res.json({
           loginSuccess: false,
           message: "The password was incorrect.",
         });
-      // 비밀번호까지 맞다면 토큰 생성.
+      // 비밀번호까지 맞다면 토큰 생성
       user.generateToken((err, user) => {
         if (err) return res.status(400).send(err);
-        // 토큰을 쿠키에 저장.
+        // 토큰을 쿠키에 저장
         res.cookie("x_auth", user.token).status(200).json({
           loginSuccess: true,
           userId: user._id,
@@ -75,10 +75,10 @@ app.post("/api/users/login", (req, res) => {
 });
 
 app.get("/api/users/auth", auth, (req, res) => {
-  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말.
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말
   res.status(200).json({
     _id: req.user._id,
-    // role이 0이면 일반유저, 그 외에는 관리자.
+    // role이 0이면 일반유저, 그 외에는 관리자
     isAdmin: req.user.role === 0 ? false : true,
     isAuth: true,
     email: req.user.email,
--- server/models/User.js
+++ server/models/User.js
@@ -36,7 +36,7 @@ const userSchema = mongoose.Schema({
 
 userSchema.pre("save", function (next) {
   var user = this;
-  // 비밀번호가 변경될 때만 실행.
+  // 비밀번호가 변경될 때만 실행
   if (user.isModified("password")) {
     // 비밀번호를 암호화시킨다.
     bcrypt.genSalt(saltRounds, function (err, salt) {
@@ -47,15 +47,15 @@ userSchema.pre("save", function (next) {
         next();
       });
     });
-    // 비밀번호 외의 것이 변경될 때 그냥 실행.
+    // 비밀번호 외의 것이 변경될 때 그냥 실행
   } else {
     next();
   }
 });
 
 userSchema.methods.comparePassword = function (plainPassword, callback) {
-  // plainPassword(123456)와 암호화된 비밀번호를 비교.
-  // 복호화가 불가하기 때문에, plainPassword를 암호화 후 비교.
+  // plainPassword(123456)와 암호화된 비밀번호를 비교
+  // 복호화가 불가하기 때문에, plainPassword를 암호화 후 비교
   bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
     if (err) return callback(err);
     callback(null, isMatch);
@@ -64,9 +64,9 @@ userSchema.methods.comparePassword = function (plainPassword, callback) {
 
 userSchema.methods.generateToken = function (callback) {
   var user = this;
-  // jsonwebtoken을 이용해서 token을 생성.
-  // user._id + "secretToken" = token을 만듦.
-  // 후에 "secretToken"으로 user._id를 찾을 수 있음.
+  // jsonwebtoken을 이용해서 token을 생성
+  // user._id + "secretToken" = token을 만듦
+  // 후에 "secretToken"으로 user._id를 찾을 수 있음
   var token = jwt.sign(user._id.toHexString(), "secretToken");
   user.token = token;
   user.save(function (err, user) {
@@ -80,7 +80,7 @@ userSchema.statics.findByToken = function (token, callback) {
   // 토큰을 decode한다.
   jwt.verify(token, "secretToken", function (err, decoded) {
     // 유저 아이디를 이용해서 유저를 찾은 다음에,
-    // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인.
+    // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
     user.findOne({ _id: decoded, token: token }, function (err, user) {
       if (err) return callback(err);
       callback(null, user);
